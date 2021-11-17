471c42155b7daeef1daa8255b602abdf5a31eed0
RolandYambao <rolandyambao@gmail.com>
2021-11-17 12:01:42 -0800

    Updated

--- index.js
+++ index.js
@@ -1,18 +1,70 @@
 // EvalError
 function evalError() {
     try {
-        throw new EvalError('Goodbye', 'noFile.js', 7);
-    } catch (ev) {
-        console.log(ev instanceof EvalError);
-        console.log(ev.message);
-        console.log(ev.name);
-        console.log(ev.fileName);
+        throw new EvalError("Eval Error");
+    }
+    catch (error) {
+        if (error instanceof EvalError) {
+            console.log("Eval Error Detected");
+        }
     }
 }
 
 // RangeError
-function rangeError() {
+function rangeError(n) {
+    if (!(n >= -500 && n <= 500)) {
+        throw new RangeError("The argument must be between -500 and 500.")
+    }
+}
+function enactRangeError() {
+    try {
+        rangeError(501);
+    }
+    catch (error) {
+        if (error instanceof RangeError) {
+            console.log("Range Error Detected")
+        }
+    }
+}
 
+// ReferenceError
+function referenceError() {
+    try {
+        let a = undefinedVariable;
+    }
+    catch (error) {
+        if (error instanceof ReferenceError) {
+            console.log("Reference Error Detected");
+        }
+    }
+}
+
+// SyntaxError
+function syntaxError() {
+    try {
+        eval("let a == 2");
+    }
+    catch (error) {
+        if (error instanceof SyntaxError) {
+            console.log("Syntax Error Detected");
+        }
+    }
+}
+
+// TypeError
+function typeError() {
+    try {
+        null.x();
+    }
+    catch (error) {
+        if (error instanceof TypeError) {
+            console.log("Type Error Detected");
+        }
+    }
 }
 
 evalError()
+enactRangeError();
+referenceError();
+syntaxError();
+typeError();
